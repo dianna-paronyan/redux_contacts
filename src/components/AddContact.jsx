f6dd8d6994e60883bf05d1679628ad163f471732
dianna1234 <01234dianna@gmail.com>
2022-07-08 17:27:17 +0400

    add validation

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -16,20 +16,43 @@ function AddContact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [number, setNumber] = useState('');
+  const [err, setErr] = useState(false);
+  const [sameEmailErr, setSameEmailErr] = useState(false);
+  const [sameNumberErr, setSameNumberErr] = useState(false);
 
 
   const contacts = useSelector((state)=> getContacts(state));
   const dispatch = useDispatch();
 
+
   const handleAddContactSubmit = (e)=>{
     e.preventDefault();
-
-    if(!name.trim() || !email.trim() || !number.trim()){
-      return
-    }
-
-      dispatch(add({name,email, number}));
-
+    
+    const checkEmail = contacts.find((contact)=> contact.email === email);
+      if(checkEmail?.email === email){
+        setSameEmailErr(true);
+        return
+      }
+      else{
+        setSameEmailErr(false)
+      }
+      const checkNumber = contacts.find((contact)=> contact.number === number);
+      if(checkNumber?.number === number){
+        setSameNumberErr(true);
+        return
+      }  else{
+        setSameNumberErr(false)
+      }
+      
+      if(!name.trim() || !email.trim() || !number.trim()){
+        setErr(true)
+        return
+      }  else{
+        setErr(false)
+      }
+      
+      
+    dispatch(add({name,email, number}));
     setName('');
     setEmail('');
     setNumber('');
@@ -41,9 +64,12 @@ function AddContact() {
       <form  onSubmit={handleAddContactSubmit} action="" style={{width:'27%',display:'flex', flexDirection:'column', gap:'20px'}}>
         <input type="text" placeholder='Name' style={styles.input} value={name} onChange={(e)=>setName(e.target.value)}  />
         <input type="email" placeholder='Email' style={styles.input} value={email} onChange={(e)=>setEmail(e.target.value)}   />
-        <input type="text" placeholder='Number' style={styles.input} value={number} onChange={(e)=>setNumber(e.target.value)}  />
+        <input type="number" placeholder='Number' style={styles.input} value={number} onChange={(e)=>setNumber(e.target.value)}  />
         <button type='submit' style={styles.input} className='addBtn'>Add Contact</button>
       </form>
+      {err ? <h4 style={{color:'red'}}>Please fill fields</h4> : null}
+      {sameEmailErr ? <h4 style={{color:'red'}}>This email already used</h4> : null}
+      {sameNumberErr ? <h4 style={{color:'red'}}>This number already used</h4> : null}
     </div>
   );
 }
